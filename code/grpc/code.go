f0417f90ad665f8b21d776adddd18058244da439
Linxhhh <1599717300@qq.com>
2024-08-13 13:55:22 +0800

    验证码服务，返回错误类型

--- code/grpc/code.go
+++ code/grpc/code.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"errors"
 
 	pb "github.com/Linxhhh/LinInk/api/proto/code"
 	"github.com/Linxhhh/LinInk/code/service"
@@ -20,10 +21,25 @@ func NewCodeServiceServer(svc *service.CodeService) *CodeServiceServer {
 
 func (server *CodeServiceServer) Send(ctx context.Context, req *pb.SendRequest) (*pb.SendResponse, error) {
 	err := server.svc.Send(ctx, req.GetBiz(), req.GetPhone())
-	return &pb.SendResponse{}, err
+	resp := pb.SendResponse{}
+	if err != nil {
+		if errors.Is(err, service.ErrSendCodeTooMany) {
+			resp.Busy = true
+		}
+	}
+	return &resp, err
 }
 
 func (server *CodeServiceServer) Verify(ctx context.Context, req *pb.VerifyRequest) (*pb.VerifyResponse, error) {
 	err := server.svc.Verify(ctx, req.GetBiz(), req.GetPhone(), req.GetInputCode())
-	return &pb.VerifyResponse{}, err
+	resp := pb.VerifyResponse{}
+	if err != nil {
+		if errors.Is(err, service.ErrVerifyCodeFailed) {
+			resp.Fail = true
+		}
+		if errors.Is(err, service.ErrVerifyCodeTooMany) {
+			resp.Busy = true
+		}
+	}
+	return &resp, err
 }
